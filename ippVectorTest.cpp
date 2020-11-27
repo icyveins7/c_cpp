8d10b4283305dc3b0d4d44a013898d8603ae867a
icyveins7 <icyveins@hotmail.com>
2020-11-27 12:53:48 +0800

    move part of ipp vector into the old test
    not fully working

--- ippVectorTest.cpp
+++ ippVectorTest.cpp
@@ -3,16 +3,25 @@
 #include <vector>
 #include <string>
 
+#define NO_DEFAULT -1
+
 namespace ippe
 {
 	const size_t INITIAL_CAP = 128;
 	
+	template <typename T>
 	class vector
 	{
-		public:
-			vector(size_t count = 0)
+		private:
+			size_t numel;
+			size_t cap;
+			size_t copylen;
+			bool reMalloc;
+			T *m_data;
+		
+			void vector_base(size_t count = 0)
 			{
-				std::cout<<"Constructing base ipp vector."<<std::endl;
+				std::cout << "Base ctor for all specialized types." << std::endl;
 				numel = count;
 				cap = INITIAL_CAP;
 				
@@ -20,7 +29,43 @@ namespace ippe
 					cap = numel;
 				}
 				reMalloc = false;
-			};
+			}
+		public:
+			vector<T>(size_t count = 0);
+			
+			// common methods
+			T* data(){
+				return m_data;
+			}
+			
+			T& back(){
+				return m_data[numel];
+			}
+			
+			T& front(){
+				return m_data[0];
+			}
+			
+			T& at(size_t pos)
+			{
+				if (pos < numel){
+					return m_data[pos];
+				}
+				else{
+					throw std::out_of_range(std::string("ippe::vector::range_check: Size is ") + std::to_string(numel));
+				}
+			}
+			
+			void push_back(T value) // for now lets not deal with lvalue/rvalue refs
+			{
+				// check size
+				if (numel == cap){
+					resize(cap * 2);
+				}
+				
+				m_data[numel] = value;
+				numel++;
+			}
 			
 			size_t size()
 			{
@@ -43,9 +88,9 @@ namespace ippe
 				else{ return false;}
 			}
 			
-			virtual void resize(size_t count)
+			void base_resize(size_t count)
 			{
-				std::cout << "Parent class resize." << std::endl;
+				std::cout << "Base class resize." << std::endl;
 				if (count > cap)
 				{
 					copylen = numel; // old, shorter length
@@ -58,83 +103,63 @@ namespace ippe
 				}
 			}
 			
+			void resize();
+			
 			~vector()
 			{
 				std::cout<<"Destructing base ipp vector."<<std::endl;
-			}
-		protected:
-			size_t numel;
-			size_t cap;
-			size_t copylen;
-			bool reMalloc;
-	};
-	
-	class vector_64fc : public vector
-	{
-		public:
-			vector_64fc()
-			{
-				std::cout<<"Constructing 64fc ipp vector."<<std::endl;
-				m_data = ippsMalloc_64fc_L(cap);
-			}
-			
-			~vector_64fc()
-			{
-				std::cout<<"Destructing 64fc ipp vector."<<std::endl;
+				
 				ippsFree(m_data);
 			}
-			
-			Ipp64fc* data(){
-				return m_data;
-			}
-			
-			Ipp64fc& back(){
-				return m_data[numel];
-			}
-			
-			Ipp64fc& front(){
-				return m_data[0];
-			}
-			
-			Ipp64fc& at(size_t pos)
-			{
-				if (pos < numel){
-					return m_data[pos];
-				}
-				else{
-					throw std::out_of_range(std::string("ippe::vector::range_check: Size is ") + std::to_string(numel));
-				}
-			}
-			
-			void push_back(Ipp64fc value) // for now lets not deal with lvalue/rvalue refs
-			{
-				// check size
-				if (numel == cap){
-					resize(cap * 2);
-				}
-				
-				m_data[numel] = value;
-				numel++;
-			}
-			
-			void resize(size_t count) override
-			{
-				vector::resize(count);
-				std::cout << "Child class resize" << std::endl;
-				if (reMalloc){
-					Ipp64fc *newm_data = ippsMalloc_64fc_L(cap);
-					ippsCopy_64fc(m_data, newm_data, copylen);
-					ippsFree(m_data);
-					m_data = newm_data;
-					
-					reMalloc = false;
-				}
-				
-			}
-		private:
-			Ipp64fc *m_data;
 	};
 	
+	// ========== specialized ctor
+	
+	// default ctor
+	template <typename T>
+	vector<T>::vector(size_t count)
+	{
+		std::cout<<"There is no default template for IPP vectors. Please specify a valid IPP type." << std::endl;
+		throw NO_DEFAULT;
+	}
+	
+	// Ipp64fc ctor
+	template <>
+	vector<Ipp64fc>::vector(size_t count)
+	{
+		vector_base(count);
+		
+		std::cout<<"Constructing Ipp64fc vector."<<std::endl;
+		
+		m_data = ippsMalloc_64fc_L(cap);
+	};
+	
+	// ========== specialized resize
+	
+	// default resize
+	template <typename T>
+	void vector<T>::resize(size_t count)
+	{
+		std::cout << "No default resize. This should never happen, should have been caught in ctor." << std::endl;
+		throw NO_DEFAULT;
+	}
+	
+	// Ipp64fc resize
+	template <>
+	void vector<Ipp64fc>::resize(size_t count)
+	{
+		base_resize(count);
+				
+		std::cout << "Ipp64fc specific resize. " << std::endl;
+		if (reMalloc){
+			Ipp64fc *newm_data = ippsMalloc_64fc_L(cap);
+			ippsCopy_64fc(m_data, newm_data, copylen);
+			ippsFree(m_data);
+			m_data = newm_data;
+			
+			reMalloc = false;
+		}
+	}
 }
 
 int main()
@@ -162,34 +187,38 @@ int main()
 	oldvector.resize(2);
 	std::cout << "std vector capacity = " << oldvector.capacity() << " and size = " << oldvector.size() << std::endl;
 	
-	
-	// create some ipp vectors?
-	ippe::vector_64fc data;
+	// create ipp template vectors
+	ippe:vector<Ipp64fc> data;
 	std::cout<<"ipp vector capacity = " << data.capacity() << " and size = " << data.size() << std::endl;
 	
-	// try a resize
-	data.resize(256);
-	data.resize(128);
-	data.resize(8);
-	std::cout<<"ipp vector capacity = " << data.capacity() << " and size = " << data.size() << std::endl;
 	
-	// pushback some data
-	Ipp64fc val = {1.0, 2.0};
-	std::cout << "pushed back value is " << val.re << ", " << val.im << std::endl;
+	// // create some ipp vectors?
+	// ippe::vector_64fc data;
+	// std::cout<<"ipp vector capacity = " << data.capacity() << " and size = " << data.size() << std::endl;
 	
-	// assign to last value
-	data.at(data.size()-1) = {10.0, 20.0};
-	// print allocate
-	for (int i = 0; i < data.size(); i++){
-		std::cout << "ipp vector val at " << i << " = " << data.at(i).re << ", " << data.at(i).im << std::endl;
-	}
-	// test exceptions
-	try{
-		data.at(data.size()) = {100.0, 200.0};
-	}
-	catch(std::out_of_range &exc){
-		std::cout<<exc.what()<<std::endl;
-	}
+	// // try a resize
+	// data.resize(256);
+	// data.resize(128);
+	// data.resize(8);
+	// std::cout<<"ipp vector capacity = " << data.capacity() << " and size = " << data.size() << std::endl;
+	
+	// // pushback some data
+	// Ipp64fc val = {1.0, 2.0};
+	// std::cout << "pushed back value is " << val.re << ", " << val.im << std::endl;
+	
+	// // assign to last value
+	// data.at(data.size()-1) = {10.0, 20.0};
+	// // print allocate
+	// for (int i = 0; i < data.size(); i++){
+		// std::cout << "ipp vector val at " << i << " = " << data.at(i).re << ", " << data.at(i).im << std::endl;
+	// }
+	// // test exceptions
+	// try{
+		// data.at(data.size()) = {100.0, 200.0};
+	// }
+	// catch(std::out_of_range &exc){
+		// std::cout<<exc.what()<<std::endl;
+	// }
 
 	return 0;
 }
