11f903fe12148f533b99ebd3b9c581bb7cba84ea
icyveins7 <icyveins@hotmail.com>
2020-12-13 22:49:18 +0800

    add specialised ctors/resizes for ippevector
    also moved all of it into ippe.h, now somewhat ready for use

--- ippVectorTest.cpp
+++ ippVectorTest.cpp
@@ -2,165 +2,7 @@
 #include "ipp.h"
 #include <vector>
 #include <string>
-
-#define NO_DEFAULT -1
-
-namespace ippe
-{
-	const size_t INITIAL_CAP = 128;
-	
-	template <typename T>
-	class vector
-	{
-		private:
-			size_t numel;
-			size_t cap;
-			size_t copylen;
-			bool reMalloc;
-			T *m_data;
-		
-			void vector_base(size_t count)
-			{
-				std::cout << "Base ctor for all specialized types." << std::endl;
-				numel = count;
-				cap = INITIAL_CAP;
-				
-				if (numel > cap){
-					cap = numel;
-				}
-				reMalloc = false;
-			}
-		public:
-			vector<T>(size_t count = 0);
-			
-			// common methods
-			T* data(){
-				return m_data;
-			}
-			
-			T& back(){
-				return m_data[numel-1];
-			}
-			
-			T& front(){
-				return m_data[0];
-			}
-			
-			T& at(size_t pos)
-			{
-				if (pos < numel){
-					return m_data[pos];
-				}
-				else{
-					throw std::out_of_range(std::string("ippe::vector::range_check: Size is ") + std::to_string(numel));
-				}
-			}
-			
-			void push_back(T value) // for now lets not deal with lvalue/rvalue refs
-			{
-				// check size
-				if (numel == cap){
-					resize(cap * 2);
-				}
-				
-				m_data[numel] = value;
-				numel++;
-			}
-			
-			size_t size()
-			{
-				return numel;
-			}
-			
-			size_t capacity()
-			{
-				return cap;
-			}
-			
-			void clear()
-			{
-				numel = 0;
-			}
-			
-			bool empty()
-			{
-				if (numel == 0){return true;}
-				else{ return false;}
-			}
-			
-			void base_resize(size_t count)
-			{
-				std::cout << "Base class resize." << std::endl;
-				if (count > cap)
-				{
-					copylen = numel; // old, shorter length
-					numel = count; // the new length
-					cap = count; // capacity = new length
-					reMalloc = true;
-				}
-				else{
-					numel = count;
-				}
-			}
-	
-			void resize(size_t count);
-			
-			~vector()
-			{
-				std::cout<<"Destructing base ipp vector."<<std::endl;
-				
-				ippsFree(m_data);
-			}
-	};
-	
-	// ========== specialized ctor
-	
-	// default ctor
-	template <typename T>
-	vector<T>::vector(size_t count)
-	{
-		std::cout<<"There is no default template for IPP vectors. Please specify a valid IPP type." << std::endl;
-		throw NO_DEFAULT;
-	}
-	
-	// Ipp64fc ctor
-	template <>
-	vector<Ipp64fc>::vector(size_t count)
-	{
-		vector_base(count);
-		
-		std::cout<<"Constructing Ipp64fc vector."<<std::endl;
-		
-		m_data = ippsMalloc_64fc_L(cap);
-	}
-	
-	// ========== specialized resize
-	
-	// default resize
-	template <typename T>
-	void vector<T>::resize(size_t count)
-	{
-		std::cout << "No default resize. This should never happen, should have been caught in ctor." << std::endl;
-		throw NO_DEFAULT;
-	}
-	
-	// Ipp64fc resize
-	template <>
-	void vector<Ipp64fc>::resize(size_t count)
-	{
-		base_resize(count);
-				
-		std::cout << "Ipp64fc specific resize. " << std::endl;
-		if (reMalloc){
-			Ipp64fc *newm_data = ippsMalloc_64fc_L(cap);
-			ippsCopy_64fc(m_data, newm_data, copylen);
-			ippsFree(m_data);
-			m_data = newm_data;
-			
-			reMalloc = false;
-		}
-	}
-}
+#include "ippe.h" // extensions header-only templates
 
 int main()
 {
@@ -226,6 +68,20 @@ int main()
 	catch(std::out_of_range &exc){
 		std::cout<<exc.what()<<std::endl;
 	}
+	
+	// test a variety of templates
+	ippe::vector<Ipp8u> v_8u;
+	ippe::vector<Ipp16u> v_16u;
+	ippe::vector<Ipp16sc> v_16sc;
+	ippe::vector<Ipp32f> v_32f;
+	ippe::vector<Ipp64f> v_64f;
+	
+	// test their resizes
+	v_8u.resize(256);
+	v_16u.resize(256);
+	v_16sc.resize(256);
+	v_32f.resize(256);
+	v_64f.resize(256);
 
 	return 0;
 }
