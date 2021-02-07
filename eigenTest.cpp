387810ea1f0d879aef2d614320ee2badb296f7b5
icyveins7 <icyveins@hotmail.com>
2021-02-07 23:13:48 +0800

    edit eigenTest to include matmul/mem alloc
    probably better to use Map for all real uses to prevent unknown memory
    allocs..

--- eigenTest.cpp
+++ eigenTest.cpp
@@ -23,12 +23,50 @@ int main()
 		data[i].im = i + 2.0;
 	}
 	
-	Map<Matrix<std::complex<double>, Dynamic, Dynamic>> mapped;
+	Map<MatrixXcd> mapped((std::complex<double>*)data, rows, cols);
 	
 	std::cout<<"mapped is init at size " << mapped.rows() << ", " << mapped.cols() << std::endl;
 	
+	std::cout << mapped << std::endl;
+	
+	// remap it as row major?
+	Map<Matrix<std::complex<double>, Dynamic, Dynamic, RowMajor>> rowmapped((std::complex<double>*)data, rows, cols);
+	
+	std::cout << "remapped as row major" << std::endl << rowmapped << std::endl;
+	
+	// make a new matrix to store matmul result
+	MatrixXcd matmul(rows, cols);
+	matmul = mapped * mapped;
+	
+	std::cout << "matmul product of mapped*mapped = " << std::endl << matmul << std::endl;
+	
+	matmul = rowmapped * rowmapped;
+	
+	std::cout << "rowmajor matmul product of mapped*mapped = " << std::endl << matmul << std::endl;
+	
+	// what if i make a wrong sized one?
+	MatrixXcd matmul_wrong(rows,cols-1);
+	
+	std::cout << "init matmul prod size at " << matmul_wrong.rows() << ", " << matmul_wrong.cols() << std::endl;
+	
+	matmul_wrong = mapped * mapped;
+	
+	std::cout << "matmul product of mapped*mapped = " << std::endl << matmul_wrong << std::endl;
+	std::cout << "but the size is now " << matmul_wrong.rows() << ", " << matmul_wrong.cols() << std::endl; // clearly this shows that a reallocation is performed for the product
+	
+	// now what if i make a wrong sized map?
+	Ipp64fc *muldata = ippsMalloc_64fc_L(rows * (cols-1));
+	
+	Map<MatrixXcd> mapmul((std::complex<double>*)muldata, rows, cols-1);
+	
+	mapmul = mapped * mapped; // here is where it fails! which is good
+	
+	std::cout << "mapped matmul product of mapped*mapped = " << std::endl << mapmul << std::endl;	
+	
+	
 	// cleanup
 	ippsFree(data);
+	ippsFree(muldata);
 	
 	return 0;
 }
