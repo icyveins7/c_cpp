cc2a7a25fbf4f237cdfd9cfe0d81eb8bd8d57804
icyveins7 <icyveins@hotmail.com>
2022-04-29 17:46:35 +0800

    complete cudaThrustTests
    looks decent to use. seems to default to async copies (which is not an
    issue, just something to note, irrelevant if only using default stream
    anyway). extraneous kernel is called at the start, may be some overhead
    but probably insignificant..

--- cudaThrustTests.cu
+++ cudaThrustTests.cu
@@ -1,3 +1,7 @@
+/*
+If kernels are not running, run deviceQuery and add flag -arch=sm_50 <- or whatever your device cuda capability is
+*/
+
 #include "cuda.h"
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
@@ -5,20 +9,66 @@
 #include "thrust/system/cuda/experimental/pinned_allocator.h" // need this specifically
 
 // namespace for experimental allocator is too long
-namespace cuta = thrust::system::cuda::experimental;
+namespace tsce = thrust::system::cuda::experimental;
 
 #define LENGTH 1000000
 
+__global__
+void myadd_kernel(float* v, int len)
+{
+    int tIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = gridDim.x * blockDim.x;
+
+    for (int i = tIdx; i < len; i = i + stride)
+    {
+        v[i] = (float)i;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    // Attempt to allocate a simple 32f host vector
-    thrust::host_vector<float, cuta::pinned_allocator<float>> hv_32f(LENGTH);
+    // Attempt to allocate a simple 32f host vector (pinned + unpinned)
+    thrust::host_vector<float, tsce::pinned_allocator<float>> hv_32f(LENGTH);
+    thrust::host_vector<float> hv_32f_unpin(LENGTH);
 
     // And a similar device vector
     thrust::device_vector<float> dv_32f(LENGTH);
 
+    // Raw allocation
+    float* darr_32f, *harr_32f;
+    cudaMalloc((void**)&darr_32f, sizeof(float) * LENGTH);
+    cudaMallocHost((void**)&harr_32f, sizeof(float) * LENGTH);
+
     // Attempt to copy over
-    dv_32f = hv_32f;
+    dv_32f = hv_32f_unpin; // note that this still causes async copies? how is that possible?
+    dv_32f = hv_32f; // async copy, as viewed in nvvp
+
+    // via explicit 'copy'
+    thrust::copy(hv_32f_unpin.begin(), hv_32f_unpin.end(), dv_32f.begin()); // this is still async copy?
+    // however, all async copies in the default stream are serialized anyway..
+
+    // Write using a kernel?
+    myadd_kernel << <2, 512 >> > ((float*)thrust::raw_pointer_cast(dv_32f.data()), dv_32f.size());
+    myadd_kernel << < 2, 512 >> > (darr_32f, LENGTH);
+
+    // Copy it back
+    hv_32f = dv_32f;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d: %f\n", i, hv_32f[i]);
+    }
+
+    // Raw copy back
+    cudaMemcpy(harr_32f, darr_32f, sizeof(float) * LENGTH, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d: %f\n", i, harr_32f[i]);
+    }
+
+    // cleanup
+    cudaFree(darr_32f);
+    cudaFreeHost(harr_32f);
 
     return 0;
 }
