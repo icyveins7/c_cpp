7d0839585537a5907b0ba46fe16de231c7fb692e
unknown <icyveins@hotmail.com>
2024-05-31 13:55:50 +0800

    update the cuda test with cmakelists and kernel fixes
    now properly compiles, runs and can be profiled correctly with nsys;
    not much diff in all 3 kernels

--- cuda_complex_shared/src/cuda_complex_shared.cu
+++ cuda_complex_shared/src/cuda_complex_shared.cu
@@ -1,12 +1,17 @@
 #include "cuComplex.h"
-#include "cuda.h"
+// #include "device_functions.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <iostream>
 
 /*
 This is the simple method of storing complex arrays into shared mem.
 It simply packs everything in as-is.
 */
-__global__ void cplxAddPackedShared(const cuComplex *x, const cuComplex *y,
-                                    cuComplex *z, int n) {
+__global__ void cplxAddPackedShared(const cuFloatComplex *x,
+                                    const cuFloatComplex *y, cuFloatComplex *z,
+                                    int n) {
 
   int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
@@ -15,15 +20,15 @@ __global__ void cplxAddPackedShared(const cuComplex *x, const cuComplex *y,
 
   // allocate each workspace to be same as blockDim
   // align one after another, i.e. well packed
-  cuComplex *s_x = (cuComplex *)s;
-  cuComplex *s_y = (cuComplex *)&s_x[blockDim.x];
+  cuFloatComplex *s_x = (cuFloatComplex *)s;
+  cuFloatComplex *s_y = (cuFloatComplex *)&s_x[blockDim.x];
 
   s_x[threadIdx.x] = x[idx];
   s_y[threadIdx.x] = y[idx];
   __syncthreads();
 
   // add and then write out
-  z[idx] = s_x[idx] + s_y[idx];
+  z[idx] = cuCaddf(s_x[threadIdx.x], s_y[threadIdx.x]);
 }
 
 /*
@@ -43,8 +48,9 @@ separate, and the reads are also separate
 
 clang-format on
 */
-__global__ void cplxAddStaggeredShared(const cuComplex *x, const cuComplex *y,
-                                       cuComplex *z, int n) {
+__global__ void cplxAddStaggeredShared(const cuFloatComplex *x,
+                                       const cuFloatComplex *y,
+                                       cuFloatComplex *z, int n) {
 
   int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
@@ -57,16 +63,26 @@ __global__ void cplxAddStaggeredShared(const cuComplex *x, const cuComplex *y,
   float *f_gap = (float *)&f_x[2 * blockDim.x];
   float *f_y = (float *)&f_gap[1];
 
-  // Cast to appropriate types
-  cuComplex *s_x = (cuComplex *)f_x;
-  cuComplex *s_y = (cuComplex *)f_y;
+  const cuFloatComplex t_x = x[idx];
+  const cuFloatComplex t_y = y[idx];
 
-  s_x[threadIdx.x] = x[idx];
-  s_y[threadIdx.x] = y[idx];
+  f_x[threadIdx.x * 2 + 0] = cuCrealf(t_x);
+  f_x[threadIdx.x * 2 + 1] = cuCimagf(t_x);
+  f_y[threadIdx.x * 2 + 0] = cuCrealf(t_y);
+  f_y[threadIdx.x * 2 + 1] = cuCimagf(t_y);
+
+  // Cast to appropriate types
+  // cuFloatComplex *s_x = (cuFloatComplex *)f_x;
+  // cuFloatComplex *s_y = (cuFloatComplex *)f_y;
+  //
+  // s_x[threadIdx.x] = x[idx];
+  // s_y[threadIdx.x] = y[idx];
   __syncthreads();
 
   // add and then write out
-  z[idx] = s_x[idx] + s_y[idx];
+  z[idx] = cuCaddf(
+      make_cuFloatComplex(f_x[threadIdx.x * 2 + 0], f_x[threadIdx.x * 2 + 1]),
+      make_cuFloatComplex(f_y[threadIdx.x * 2 + 0], f_y[threadIdx.x * 2 + 1]));
 }
 
 /*
@@ -108,9 +124,9 @@ Then 2 addition instructions are executed, 1 for the reals and 1 for the imags.
 
 So in this case it feels more explicit(?) that the instruction count is lower.
 */
-__global__ void cplxAddDeinterleavedShared(const cuComplex *x,
-                                           const cuComplex *y, cuComplex *z,
-                                           int n) {
+__global__ void cplxAddDeinterleavedShared(const cuFloatComplex *x,
+                                           const cuFloatComplex *y,
+                                           cuFloatComplex *z, int n) {
 
   int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
@@ -125,17 +141,17 @@ __global__ void cplxAddDeinterleavedShared(const cuComplex *x,
   float *s_y_i = (float *)&s_y_r[blockDim.x];
 
   // Read to thread-local contiguously first
-  float s_x = x[idx];
+  cuFloatComplex s_x = x[idx];
 
   // Then split
-  s_x_r[threadIdx.x] = s_x.real();
-  s_x_i[threadIdx.x] = s_x.imag();
+  s_x_r[threadIdx.x] = s_x.x;
+  s_x_i[threadIdx.x] = s_x.y;
 
   // Similar for y
-  float s_y = y[idx];
+  cuFloatComplex s_y = y[idx];
 
-  s_y_r[threadIdx.x] = s_y.real();
-  s_y_i[threadIdx.x] = s_y.imag();
+  s_y_r[threadIdx.x] = s_y.x;
+  s_y_i[threadIdx.x] = s_y.y;
 
   __syncthreads();
 
@@ -143,33 +159,84 @@ __global__ void cplxAddDeinterleavedShared(const cuComplex *x,
   float z_r = s_x_r[threadIdx.x] + s_y_r[threadIdx.x];
   float z_i = s_x_i[threadIdx.x] + s_y_i[threadIdx.x];
 
-  z[idx] = cuComplex(z_r, z_i);
+  z[idx] = make_cuFloatComplex(z_r, z_i);
 }
 int main() {
   constexpr int LENGTH = 1000000;
 
   // Raw allocation
-  cuComplex *d_x, *d_y, *d_z, *h_x, *h_y, *h_z;
-  cudaMalloc((void **)&d_x, sizeof(cuComplex) * LENGTH);
-  cudaMalloc((void **)&d_y, sizeof(cuComplex) * LENGTH);
-  cudaMalloc((void **)&d_z, sizeof(cuComplex) * LENGTH);
-  cudaMallocHost((void **)&h_x, sizeof(cuComplex) * LENGTH);
-  cudaMallocHost((void **)&h_y, sizeof(cuComplex) * LENGTH);
-  cudaMallocHost((void **)&h_z, sizeof(cuComplex) * LENGTH);
+  cuFloatComplex *d_x, *d_y, *d_z, *h_x, *h_y, *h_z;
+  cudaMalloc((void **)&d_x, sizeof(cuFloatComplex) * LENGTH);
+  cudaMalloc((void **)&d_y, sizeof(cuFloatComplex) * LENGTH);
+  cudaMalloc((void **)&d_z, sizeof(cuFloatComplex) * LENGTH);
+  cudaMallocHost((void **)&h_x, sizeof(cuFloatComplex) * LENGTH);
+  cudaMallocHost((void **)&h_y, sizeof(cuFloatComplex) * LENGTH);
+  cudaMallocHost((void **)&h_z, sizeof(cuFloatComplex) * LENGTH);
+
+  // Fill some values
+  for (int i = 0; i < LENGTH; ++i) {
+    h_x[i] = make_cuFloatComplex((float)i, (float)i);
+    h_y[i] = make_cuFloatComplex((float)i, (float)i);
+  }
 
   // Raw copy
-  cudaMemcpy(d_x, h_x, sizeof(cuComplex) * LENGTH, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, h_y, sizeof(cuComplex) * LENGTH, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_x, h_x, sizeof(cuFloatComplex) * LENGTH, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, h_y, sizeof(cuFloatComplex) * LENGTH, cudaMemcpyHostToDevice);
 
   // kernels
   constexpr int THREADS_PER_BLK = 128;
   const int numBlks = LENGTH / THREADS_PER_BLK;
-  cplxAddPackedShared<<<numBlks, THREADS_PER_BLK, >>>(d_x, d_y, d_z, LENGTH);
-  cplxAddStaggeredShared<<<numBlks, THREADS_PER_BLK, >>>(d_x, d_y, d_z, LENGTH);
+  printf("numBlks = %d\n", numBlks);
+  // in all cases overallocate the shared mem
+
+  // first kernel
+  cplxAddPackedShared<<<numBlks, THREADS_PER_BLK,
+                        THREADS_PER_BLK * sizeof(cuFloatComplex) * 3>>>(
+      d_x, d_y, d_z, LENGTH);
 
   // Raw copy back
-  cudaMemcpy(h_z, d_z, sizeof(cuComplex) * LENGTH, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_z, d_z, sizeof(cuFloatComplex) * LENGTH, cudaMemcpyDeviceToHost);
 
+  for (int i = 0; i < 5; ++i) {
+    printf("%f, %f + %f, %f = %f, %f\n", h_x[i].x, h_x[i].y, h_y[i].x, h_y[i].y,
+           h_z[i].x, h_z[i].y);
+
+    // reset values
+    h_z[i].x = 0.0f;
+    h_z[i].y = 0.0f;
+  }
+
+  // second kernel
+  cplxAddStaggeredShared<<<numBlks, THREADS_PER_BLK,
+                           THREADS_PER_BLK * sizeof(cuFloatComplex) * 3>>>(
+      d_x, d_y, d_z, LENGTH);
+  // Raw copy back
+  cudaMemcpy(h_z, d_z, sizeof(cuFloatComplex) * LENGTH, cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < 5; ++i) {
+    printf("%f, %f + %f, %f = %f, %f\n", h_x[i].x, h_x[i].y, h_y[i].x, h_y[i].y,
+           h_z[i].x, h_z[i].y);
+
+    // reset values
+    h_z[i].x = 0.0f;
+    h_z[i].y = 0.0f;
+  }
+
+  // third kernel
+  cplxAddDeinterleavedShared<<<numBlks, THREADS_PER_BLK,
+                               THREADS_PER_BLK * sizeof(cuFloatComplex) * 3>>>(
+      d_x, d_y, d_z, LENGTH);
+  // Raw copy back
+  cudaMemcpy(h_z, d_z, sizeof(cuFloatComplex) * LENGTH, cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < 5; ++i) {
+    printf("%f, %f + %f, %f = %f, %f\n", h_x[i].x, h_x[i].y, h_y[i].x, h_y[i].y,
+           h_z[i].x, h_z[i].y);
+
+    // reset values
+    h_z[i].x = 0.0f;
+    h_z[i].y = 0.0f;
+  }
   // cleanup
   cudaFree(d_x);
   cudaFree(d_y);
@@ -177,6 +244,8 @@ int main() {
   cudaFreeHost(h_x);
   cudaFreeHost(h_y);
   cudaFreeHost(h_z);
+
+  std::cout << "Complete\n";
 
   return 0;
 }
