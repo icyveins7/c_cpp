8e68590b2ab7e3452d2267fe6ffae9332ddb1e34
icyveinsphone <icyveins@hotmail.com>
2026-04-22 11:23:08 +0000

    add base/offset diff and lerp calcs to restore double precision

--- float_but_double.c
+++ float_but_double.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 
+struct BaseOffset{
+  int m;
+  float c;
+};
+
+void initBaseOffset(struct BaseOffset* ba, int _m, int x){
+  ba->m = _m;
+  ba->c = x - _m;
+}
+
+double baseOffsetAsDouble(struct BaseOffset* ba){
+    return (double)ba->m + (double)ba->c;
+  }
+
+double diff(int a, int b){
+  struct BaseOffset ba;
+  initBaseOffset(&ba, a, b);
+  return baseOffsetAsDouble(&ba);
+}
+
+double lerp(int a, int b, float x){
+  struct BaseOffset ba;
+  initBaseOffset(&ba, a, b);
+  ba.c *= x;
+  return baseOffsetAsDouble(&ba);
+}
+
 int main(){
   printf("test floats emulating doubles\n");
 
@@ -29,7 +56,13 @@ int main(){
   printf("m = %d\nxc = %d\nyc = %d\n", m, xc, yc);
   printf("xcf = %12.8f\nycf = %12.8f\nzcf = %12.8f\n", xcf, ycf, zcf);
 
-  
+  double zdd = diff(x, y);
+  printf("zdd = %12.8f\n", zdd);
+
+  float k = 0.123f;
+  double zl = xd + (yd-xd)*k;
+  double zll = lerp(x, y, k);
+  printf("zl = %12.8f\nzll = %12.8f\n", zl, zll);
 
   return 0;
 }
